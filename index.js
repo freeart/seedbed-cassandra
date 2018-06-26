2a102d20e26788b040c55588640bfeba62a6cd52
Pavel Nekrasov <pavel@resultdevelopment.com>
2018-06-26 16:18:47 +0200

    first implement

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-const assert = require('assert')
+const assert = require('assert'),
+	cassandra = require('cassandra-driver');
 
 module.exports = function () {
 	assert(!this.cassandra, "field exists")
 
-
-	this.on("ready", () => {
-
-	});
+	this.cassandra = new cassandra.Client({ contactPoints: ['localhost'], keyspace: 'ref' });
 
 	return Promise.resolve();
 }
